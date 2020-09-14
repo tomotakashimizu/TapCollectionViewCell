a27d200d1d37fb168fc9a385102709d55909e827
tomotakashimizu <tomotaka.developer@gmail.com>
2020-09-15 01:08:58 +0900

    Initial Commit

---
+++ TapCollectionViewCellSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapCollectionViewCellSample
+//
+//  Created by 清水智貴 on 2020/09/15.
+//  Copyright © 2020 com.tomotaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
